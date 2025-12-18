bbfe2a83562e7e5e6847ce4f6fc62d5cda49f245
Cruelasss <irula5198@gmail.com>
2025-12-18 19:18:38 +0700

    tambah objek DestinasiHome untuk navigasi halaman utama

---
+++ app/src/main/java/com/example/questfirebase_249/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_249.uicontroller.route
+
+import com.example.QuestFirebase_249.R
+import com.example.questfirebase_249.uicontroller.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

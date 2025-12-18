214e7c7b55f6b5ff64fc3b59b215ab872b168f00
Cruelasss <irula5198@gmail.com>
2025-12-18 19:23:41 +0700

    setup MainActivity sebagai entry point aplikasi

--- app/src/main/java/com/example/questfirebase_249/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_249/MainActivity.kt
@@ -7,21 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.questfirebase_249.ui.theme.QuestFirebase_249Theme
+import com.example.questfirebase_249.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuestFirebase_249Theme {
+            QuestFirebase_249Theme{
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +28,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestFirebase_249Theme {
-        Greeting("Android")
-    }
-}

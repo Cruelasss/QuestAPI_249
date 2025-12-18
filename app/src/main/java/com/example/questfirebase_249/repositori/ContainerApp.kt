da00b8d43f699ee216f39de9144bf7eba769f89b
Cruelasss <irula5198@gmail.com>
2025-12-18 14:13:11 +0700

    implementasi ContainerApp untuk dependency injection

---
+++ app/src/main/java/com/example/questfirebase_249/repositori/ContainerApp.kt
@@ -0,0 +1,29 @@
+package com.example.questfirebase_249.repositori
+
+
+import android.app.Application
+import com.example.questfirebase_249.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http:10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply{
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+
+}

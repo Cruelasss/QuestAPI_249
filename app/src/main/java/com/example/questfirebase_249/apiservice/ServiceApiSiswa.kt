f27a6a70d8235735a422f4e8c116965a905c5ee9
Cruelasss <irula5198@gmail.com>
2025-12-18 13:52:06 +0700

    tambah interface ServiceApiSiswa untuk integrasi API

--- app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
@@ -1,3 +1,15 @@
 package com.example.questfirebase_249.apiservice
 
+import com.example.questfirebase_249.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+}

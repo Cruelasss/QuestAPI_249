0e97b498cc3bd7b70c8bf87d4efb2f3105f1b0e3
Cruelasss <irula5198@gmail.com>
2025-12-18 19:17:57 +0700

    tambah objek DestinasiEntry untuk navigasi entry siswa

---
+++ app/src/main/java/com/example/questfirebase_249/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_249.uicontroller.route
+
+import com.example.QuestFirebase_249.R
+import com.example.questfirebase_249.uicontroller.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}

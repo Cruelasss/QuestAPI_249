620c7fea86c88b49d7c828cb9dc28ce1193ad629
Cruelasss <irula5198@gmail.com>
2025-12-25 22:40:42 +0700

    feat: tambahkan input OutlinedTextField untuk nama siswa

--- app/src/main/java/com/example/questfirebase_249/view/EntrySiswaScreen.kt
+++ app/src/main/java/com/example/questfirebase_249/view/EntrySiswaScreen.kt
@@ -108,3 +108,11 @@ fun FormTambahSiswa(
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = { onValueChange(detailSiswa.copy(nama = it)) },
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true

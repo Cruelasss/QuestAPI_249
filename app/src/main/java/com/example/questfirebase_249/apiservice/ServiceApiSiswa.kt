1d374ef8a938e270dc088d447267e84a8246ddbb
Cruelasss <irula5198@gmail.com>
2025-12-25 19:09:41 +0700

    feat: tambahkan endpoint GET, PUT, dan DELETE pada interface API Siswa

--- app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.questfirebase_249.apiservice
 
 import com.example.questfirebase_249.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 
 interface ServiceApiSiswa {
@@ -12,4 +15,14 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET ("baca1Teman.php/{id}")
+    suspend fun  getSatuSiswa(@Query("id") id : Int) : DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id : Int, @Body dataSiswa: DataSiswa) :retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id : Int) :retrofit2.Response<Void>
+
 }

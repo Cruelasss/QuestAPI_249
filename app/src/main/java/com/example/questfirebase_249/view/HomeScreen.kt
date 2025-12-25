e8ab48f15912c62a9e82233dea3e2e501cad0b40
Cruelasss <irula5198@gmail.com>
2025-12-25 22:48:24 +0700

    tambah ikon Phone pada HomeScreen

--- app/src/main/java/com/example/questfirebase_249/view/HomeScreen.kt
+++ app/src/main/java/com/example/questfirebase_249/view/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults

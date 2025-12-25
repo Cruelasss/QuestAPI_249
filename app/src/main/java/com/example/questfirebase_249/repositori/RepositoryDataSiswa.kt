e30934a7ed8741b608f705355102c91d5524a2df
Cruelasss <irula5198@gmail.com>
2025-12-25 19:12:21 +0700

    feat: implementasi repositori JaringanRepositoryDataSiswa untuk manajemen data siswa

--- app/src/main/java/com/example/questfirebase_249/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questfirebase_249/repositori/RepositoryDataSiswa.kt
@@ -6,11 +6,24 @@ import com.example.questfirebase_249.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa {
+): RepositoryDataSiswa {
     override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa
+        .hapusSatuSiswa(id)
 }

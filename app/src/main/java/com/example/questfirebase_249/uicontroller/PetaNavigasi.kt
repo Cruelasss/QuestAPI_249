264e9f01f92e1448937cd3104b34cac84bf7b221
Cruelasss <irula5198@gmail.com>
2025-12-25 20:02:19 +0700

    feat: atur grafik navigasi (NavHost) untuk menghubungkan antar halaman aplikasi

--- app/src/main/java/com/example/questfirebase_249/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_249/uicontroller/PetaNavigasi.kt
@@ -3,57 +3,53 @@ package com.example.questfirebase_249.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questfirebase_249.uicontroller.route.DestinasiDetail
+import com.example.questfirebase_249.uicontroller.route.DestinasiEdit
 import com.example.questfirebase_249.uicontroller.route.DestinasiEntry
 import com.example.questfirebase_249.uicontroller.route.DestinasiHome
+import com.example.questfirebase_249.view.DetailSiswaScreen
+import com.example.questfirebase_249.view.EditSiswaScreen
 import com.example.questfirebase_249.view.EntrySiswaScreen
 import com.example.questfirebase_249.view.HomeScreen
 
 @Composable
-fun DataSiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    HostNavigasi(navController = navController, modifier = modifier)
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
 }
 
 @Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = modifier
-    ) {
-        // Rute untuk Halaman Utama
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier
+    ){
         composable(DestinasiHome.route) {
-            HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                },
-                // Perbaikan Error: Nama parameter harus 'navigateToItemUpdate' sesuai HomeScreen.kt
-                // Dan tambahkan tanda koma setelah navigateToItemEntry
-                navigateToItemUpdate = {
-                    // navController.navigate("${DestinasiDetail.route}/$itemId")
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onItemClick = {
+                    navController.navigate("${DestinasiDetail.route}/$it")
                 }
             )
         }
-
-        // Rute untuk Halaman Entry Siswa
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = {
-                    navController.popBackStack() // Gunakan popBackStack untuk kembali dengan benar
-                },
-                // Perbaikan Error: Parameter 'onNavigateUp' wajib diisi sesuai HalamanEntry.jpg
-                onNavigateUp = {
-                    navController.navigateUp()
-                }
-            )
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
         }
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+            type = NavType.IntType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg)
+        ){
+            type = NavType.IntType})){
+        EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+            onNavigateUp = { navController.navigateUp() })
+    }
     }
 }

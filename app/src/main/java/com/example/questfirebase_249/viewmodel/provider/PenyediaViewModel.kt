cc3398dabf7c9dc5decd4d2c7f096d81c199db3f
Cruelasss <irula5198@gmail.com>
2025-12-25 20:02:12 +0700

    feat: tambahkan factory untuk menyediakan instance ViewModel ke dalam UI

--- app/src/main/java/com/example/questfirebase_249/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_249/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.questfirebase_249.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_249.repositori.AplikasiDataSiswa
-
+import com.example.questfirebase_249.viewmodel.DetailViewModel
+import com.example.questfirebase_249.viewmodel.EditViewModel
+import com.example.questfirebase_249.viewmodel.EntryViewModel
+import com.example.questfirebase_249.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
@@ -13,13 +17,17 @@ fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer {
-            _root_ide_package_.com.example.questfirebase_249.viewmodel.HomeViewModel(
+            DetailViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
         initializer {
-            _root_ide_package_.com.example.questfirebase_249.viewmodel.EntryViewModel(
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }

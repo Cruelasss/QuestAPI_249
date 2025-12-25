8dea55d75e6ddd6a1c06d9d9df893e789df44701
Cruelasss <irula5198@gmail.com>
2025-12-25 22:51:50 +0700

    tambah import Response di ServiceApiSiswa

--- app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questfirebase_249/apiservice/ServiceApiSiswa.kt
@@ -1,6 +1,7 @@
 package com.example.questfirebase_249.apiservice
 
 import com.example.questfirebase_249.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
